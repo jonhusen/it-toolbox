0837e55578caefea17ffd5e280a2150c46794903
Jon Husen <jon.husen@gmail.com>
2019-03-22 16:52:58 -0400

    Add initial scripts

---
+++ powershell/Connect-ExchangeOnline.ps1
@@ -0,0 +1,17 @@
+﻿#O365 connection script
+
+#O365 Credentials
+$O365Cred = Get-Credential
+
+#Create connection to Exchange Online organization
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365Cred -Authentication Basic -AllowRedirection
+
+#Load Exchange cmdlets on local computer
+
+Import-PSSession $Session -DisableNameChecking
+
+# Close the connection
+Remove-PSSession $Session
+
+# If account is using MFA, you must run this command in the Exchange Online Powershell module
+Connect-EXOPSSession -UserPrincipalName $O365Cred.UserName
---
+++ powershell/StoredCredential.ps1
@@ -0,0 +1,50 @@
+﻿
+
+
+# simple password vault access class
+class StoredCredential{
+    [System.Management.Automation.PSCredential] $PSCredential
+    [string] $account;
+    [string] $password;
+
+    # loads credential from vault
+    StoredCredential( [string] $name ){
+        [void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime] 
+        $vault = New-Object Windows.Security.Credentials.PasswordVault 
+        $cred = $vault.FindAllByResource($name) | select -First 1
+        $cred.retrievePassword()
+        $this.account = $cred.userName
+        $this.password = $cred.password
+        $pwd_ss = ConvertTo-SecureString $cred.password -AsPlainText -Force
+        $this.PSCredential = New-Object System.Management.Automation.PSCredential ($this.account, $pwd_ss )
+    }
+
+    static [bool] Exists( [string] $name ){
+        [void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime] 
+        $vault = New-Object Windows.Security.Credentials.PasswordVault 
+        try{
+            $vault.FindAllByResource($name)
+        }
+        catch{
+            if ( $_.Exception.message -match "element not found" ){
+                return $false
+            }
+            throw $_.exception
+        }
+        return $true
+    }
+
+
+    static [StoredCredential] Store( [string] $name, [string] $login, [string] $pwd ){
+        [void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
+        $vault=New-Object Windows.Security.Credentials.PasswordVault
+        $cred=New-Object Windows.Security.Credentials.PasswordCredential($name, $login, $pwd)
+        $vault.Add($cred)
+        return [StoredCredential]::new($name)
+    }
+    
+    static [StoredCredential] Store( [string] $name, [PSCredential] $pscred ){
+        return [StoredCredential]::Store( $name, $pscred.UserName, ($pscred.GetNetworkCredential()).Password )
+    }
+
+}  # class StoredCredential
